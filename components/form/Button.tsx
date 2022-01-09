592fc8637015bf1fc241e3224d1f48bcf5abc3b7
Jose Antonio Home Freela <joseantonio1@hotmai.com.br>
2022-01-09 10:33:18 -0300

    added small button opt

--- components/form/Button.tsx
+++ components/form/Button.tsx
@@ -1,10 +1,10 @@
 import styles from '../../styles/Button.module.scss';
 
-const Button = ({ type, handleClick=null, value, loading=false, small=false }) => {
+const Button = ({ type, handleClick=null, value, loading=false, small=false, leng=null }) => {
   return (
     loading ?
     <div>Carregando...</div> :
-    <input className={`${styles.btn} ${small ? styles.small : ''}`} type={type} onClick={(e) => handleClick ? handleClick(e) : null} value={value} />
+    <input className={`${styles.btn} ${small ? styles.small : ''}`} style={{ width: leng ? leng : "100%" }} type={type} onClick={(e) => handleClick ? handleClick(e) : null} value={value} />
   )
 }
 
